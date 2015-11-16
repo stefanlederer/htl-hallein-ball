0f3f8933cf24df2b8dbc0fb8b4d616aec10bba86
TobiasFranek <tobias.franek@gmail.com>
2015-11-16 17:25:08 +0100

    fixed infos style and js + changed headline homepage text

--- src/js/content.js
+++ src/js/content.js
@@ -12,15 +12,26 @@ App.content.prototype = {
     },
     bindEvents: function () {
         $(window).resize(function () {
-            $('#header').height($(window).height() - $('#nav').height());
+            $('.big-header').height($(window).height() - $('#nav').height());
             $('#container').height($(window).height());
-            $('#content').css('top', $(window).height());
-            $('.confetti').height($('#content').height());
+            if ($('#header').hasClass('big-header')) {
+                $('#content').css('top', $(window).height());
+            }
+            $('.confetti').height($('#content').height() + 50);
         });
         $('.fly-down, .fa-angle-down').click(function () {
             $('#container').animate({
                 scrollTop: $(window).height() - $('#nav').height()
             }, 1000)
+        });
+        $('#container').scroll(function () {
+            if($('#container').scrollTop() === 0){
+                $('.infos').removeClass('opacity0');
+                $('.infos').addClass('opacity1');
+            }else {
+                $('.infos').removeClass('opacity1');
+                $('.infos').addClass('opacity0');
+            }
         });
     }
 };
